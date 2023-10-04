dc7a557cbce1b326d8317587e975f3f1ce7d39b3
Wendigo01 <veress.martin@students.jedlik.eu>
2023-10-04 11:30:26 +0200

    init 2

--- script.js
+++ script.js
@@ -1,49 +1,90 @@
 let urlap = document.querySelector('.urlap');
-let tablazat = document.querySelector('.table');
+let tablazat = document.querySelector('.tablazat').getElementsByTagName('tbody')[0];
 
-let dolgozok = [];
+let dolgozok = []; // üres tömb
 
 function hozzaad(event){
-    event.preventDefault();     // nem engedi újratölteni az oldalt
+    event.preventDefault(); // nem engedi újratöltődni az oldalt
     // console.log('OK');
-    let dolgozo = {};   //üres objektum
+    let dolgozo = {}; // üres objektum
     dolgozo.nev = document.querySelector('#nev').value;
     dolgozo.email = document.querySelector('#email').value;
-    dolgozo.szuletesiDatum = document.querySelector('szuletesi-datum').value;
-    dolgozo.neme = document.querySelector('input[name="name"]:checked').value;
+    dolgozo.szuletesiDatum = document.querySelector('#szuletesi-datum').value;
+    dolgozo.neme = document.querySelector('input[name="neme"]:checked').value;
 
-    dolgozok.push(dolgozo); //.push() a végén bővíti a tömböt
+    //console.log(dolgozo);
+    dolgozok.push(dolgozo); // .push() a végén bővíti a tömböt
+    //console.log(dolgozok);
+    listazas();
+}
+
+function torles(event){
+    console.log('ok',event.target.dataset.id);
+    dolgozok.splice(event.target.dataset.id,1);
+    listazas();
 }
 
 function listazas(){
     let i = 1;
-    tablazat.innerHTML='';
-    dolgozok.forEach(egyDolgozo =>{
-        let tr = document.createElement('tr');
+    tablazat.innerHTML=''; // törli a tbody tartalmát
+    dolgozok.forEach(egyDolgozo => {
+        let tr = document.createElement('tr');  // <tr></tr>
 
-        let td = document.createElement('td');
-        td.textContent = i;
-        tr.append(td);
+        let td = document.createElement('td'); // <td></td>
+        td.textContent = i;        // <td>1</td>
+        tr.append(td); 
 
-        td = document.createElement('td');
-        td.textContent = egyDolgozo.nev;
-        tr.append(td);
+        td = document.createElement('td'); // <td></td>
+        td.textContent = egyDolgozo.nev;        // <td>Béla</td>
+        tr.append(td); // a végén bővíti a html elemet <tr><td>Béla</td></tr>
 
-        td = document.createElement('td');
-        td.textContent = egyDolgozo.email;
-        tr.append(td);
+        td = document.createElement('td'); 
+        td.textContent = egyDolgozo.email;       
+        tr.append(td); 
 
-        td = document.createElement('td');
-        td.textContent = egyDolgozo.szuletesiDatum;
-        tr.append(td);
+        td = document.createElement('td'); 
+        td.textContent = egyDolgozo.szuletesiDatum;       
+        tr.append(td); 
 
-        td = document.createElement('td');
-        td.textContent = egyDolgozo.neme;
+        td = document.createElement('td'); 
+        td.textContent = egyDolgozo.neme;       
+        tr.append(td); 
+
+        let button =  document.createElement('button'); // <button></button>
+        button.textContent = 'Törlés';                  // <button>Törlés</button>
+        button.className = 'btn btn-danger';
+        button.dataset.id = i-1;
+        button.addEventListener('click',torles)
+        td = document.createElement('td'); 
+        td.append(button);                              // <td><button>Törlés</button></td>
         tr.append(td);
 
         tablazat.append(tr);
         i++;
-    })
+    });
 }
 
-urlap.addEventListener('submit', hozzaad);
+// eseménykezelés
+urlap.addEventListener('submit',hozzaad);
+
+
+// teszt adatok
+dolgozok.push({
+    nev : 'Józsi',
+    email: 'jozsi@cegem.hu',
+    szuletesiDatum: '1980.01.01',
+    neme: 'fiú'
+});
+dolgozok.push({
+    nev : 'Béla',
+    email: 'bela@cegem.hu',
+    szuletesiDatum: '1984.11.01',
+    neme: 'fiú'
+});
+dolgozok.push({
+    nev : 'Kata',
+    email: 'kata@cegem.hu',
+    szuletesiDatum: '1985.10.02',
+    neme: 'lány'
+});
+listazas();
